28b96642d55b9dec61158d169fe35d643ed8c235
wjl077 <1078425984@qq.com>
2023-02-08 22:48:05 +0800

    完成原始波形和频谱图的多线条展示

--- src/app/detail-realtime/detail-realtime.component.ts
+++ src/app/detail-realtime/detail-realtime.component.ts
@@ -36,6 +36,7 @@ export class DetailRealtimeComponent implements OnInit {
   equipmentID = ''
 
   ngOnInit(): void {
+
     this.route.params.subscribe((data: any) => {
       this.equipmentID = data.EquipmentID
       //初始化加载统计信息
@@ -122,131 +123,170 @@ export class DetailRealtimeComponent implements OnInit {
 
   ];
 
+  //原始波形
+  signal = {
+    tooltip: {
+      trigger: 'axis',
+      // position: function (pt) {
+      //   return [pt[0], '10%'];
+      // }
+    },
+    toolbox: {
+      feature: {
+        dataZoom: {
+          yAxisIndex: 'none'
+        },
+        restore: {},
+        saveAsImage: {}
+      }
+    },
+    title: {
+      text: '原始波形',
+    },
+    animation: false,
+    xAxis: {
+      type: 'category',
+    },
+    yAxis: {
+      type: 'value',
+      data: 'value',
+      // boundaryGap: [0, '100%']
+    },
+    dataZoom: [
+      {
+        type: 'inside',
+        start: 0,
+        end: 100
+      },
+      {
+        start: 0,
+        end: 100
+      }
+    ],
+    series: [
+      {
+        name:'',
+        sampling: 'lttb',
+        data: [],
+        type: 'line',
+        tooltip: {
+          trigger: 'item'
+        },
+      },
+    ]
+  };
+  fft = {
+    // color: '#91cc75',
+    tooltip: {
+      trigger: 'axis',
+      // position: function (pt) {
+      //   return [pt[0], '10%'];
+      // }
+    },
+    toolbox: {
+      feature: {
+        dataZoom: {
+          yAxisIndex: 'none'
+        },
+        restore: {},
+        saveAsImage: {}
+      }
+    },
+    title: {
+      text: '频谱图',
+    },
+    animation: false,
+    xAxis: {
+      type: 'category',
+    },
+    yAxis: {
+      type: 'value',
+      data: 'value',
+      // boundaryGap: [0, '100%']
+    },
+    dataZoom: [
+      {
+        type: 'inside',
+        start: 0,
+        end: 100
+      },
+      {
+        start: 0,
+        end: 100
+      }
+    ],
+    series: [
+      {
+        name:'',
+        sampling: 'lttb',
+        data: [],
+        type: 'line',
+        tooltip: {
+          trigger: 'item'
+        },
+      }
+    ]
+  };
+
   checkBoxValues1 = [];
 
   onCheckbox1Change(value: any) {
     console.log(this.checkBoxValues1)
+    this.signal.series=[]
+    //初始化原始波形
+    var chart1 = document.getElementsByClassName("OriginalWaveform")
+    var LineChart1 = echart.init(chart1[0] as HTMLDivElement)
+    LineChart1.clear()
+    LineChart1.setOption(this.signal)
 
+    this.fft.series=[]
+    //初始化频谱图
+    var chart2 = document.getElementsByClassName("Spectrogram")
+    var LineChart2 = echart.init(chart2[0] as HTMLDivElement)
+    LineChart2.clear()
+    LineChart2.setOption(this.fft)
 
-
-
-
-    this.http.get("/api/realtime/" + this.equipmentID + "/" + this.checkBoxValues1[0]).subscribe((res: any) => {
-      //初始化原始波形
-      var chart1 = document.getElementsByClassName("OriginalWaveform")
-      var LineChart1 = echart.init(chart1[0] as HTMLDivElement)
-
-      //原始波形
-      var signal = {
-        tooltip: {
-          trigger: 'axis',
-          // position: function (pt) {
-          //   return [pt[0], '10%'];
-          // }
-        },
-        toolbox: {
-          feature: {
-            dataZoom: {
-              yAxisIndex: 'none'
-            },
-            restore: {},
-            saveAsImage: {}
-          }
-        },
-        title: {
-          text: '原始波形',
-        },
-        animation: false,
-        xAxis: {
-          type: 'category',
-        },
-        yAxis: {
-          type: 'value',
-          data: 'value',
-          // boundaryGap: [0, '100%']
-        },
-        dataZoom: [
-          {
-            type: 'inside',
-            start: 0,
-            end: 100
+    for(var i=0;i<this.checkBoxValues1.length;i++){
+      this.http.get("/api/realtime/" + this.equipmentID + "/" + this.checkBoxValues1[i]).subscribe((res: any) => {
+        var series1={
+          name:'',
+          sampling: 'lttb',
+          data: [],
+          type: 'line',
+          tooltip: {
+            trigger: 'item'
           },
-          {
-            start: 0,
-            end: 100
-          }
-        ],
-        series: [
-          {
-            sampling: 'lttb',
-            data: res.data.signal,
-            type: 'line',
-            tooltip: {
-              trigger: 'item'
-            },
-          }
-        ]
-      };
-      LineChart1.setOption(signal)
+        }
+        series1.name=res.name
+        series1.data=res.data.signal
+        this.signal.series.push(series1)
+        // console.log(this.signal.series)
+        LineChart1.setOption(this.signal)
 
-      //初始化频谱图
-      var chart2 = document.getElementsByClassName("Spectrogram")
-      var LineChart2 = echart.init(chart2[0] as HTMLDivElement)
-
-      var fft = {
-        color: '#91cc75',
-        tooltip: {
-          trigger: 'axis',
-          // position: function (pt) {
-          //   return [pt[0], '10%'];
-          // }
-        },
-        toolbox: {
-          feature: {
-            dataZoom: {
-              yAxisIndex: 'none'
-            },
-            restore: {},
-            saveAsImage: {}
-          }
-        },
-        title: {
-          text: '频谱图',
-        },
-        animation: false,
-        xAxis: {
-          type: 'category',
-        },
-        yAxis: {
-          type: 'value',
-          data: 'value',
-          // boundaryGap: [0, '100%']
-        },
-        dataZoom: [
-          {
-            type: 'inside',
-            start: 0,
-            end: 100
+        var series2={
+          name:'',
+          sampling: 'lttb',
+          data: [],
+          type: 'line',
+          tooltip: {
+            trigger: 'item'
           },
-          {
-            start: 0,
-            end: 100
-          }
-        ],
-        series: [
-          {
-            sampling: 'lttb',
-            data: res.data.fft,
-            type: 'line',
-            tooltip: {
-              trigger: 'item'
-            },
-          }
-        ]
-      };
-      LineChart2.setOption(fft)
+        }
+        series2.name=res.name
+        series2.data=res.data.fft
+        this.fft.series.push(series2)
+        LineChart2.setOption(this.fft)
 
-    })
+
+      })
+    }
+
+
+
+
+
+
+
 
 
   }
